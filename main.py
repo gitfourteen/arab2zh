783a0afe78fe1fb052cfebaafd40d02b60542c72
gitfourteen <22335780+gitfourteen@users.noreply.github.com>
2020-08-30 12:46:44 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Transform Arab number of base 10 format to Simple Chinese way. By @xiaobit
+0. number range ±e17, '十万兆', extremes not inclusive, can expand if need.
+1. transform '± 2020.0' as '正负二千零二十点零'
+2. not support scientific format, like 1.0e-1
+3. ON/OFF insignificant figures
+
+More exampls:
+' ± -000.000100'            >> 正负零点零零零一零零
+'10101010101'               >> 一百零一亿零一百零一万零一百零一
+'-10080038703101.0'         >> 负十兆零八百亿零三千八百七十万三千一百零一点零
+'-12970000000001001.1'      >> 负一万二千九百七十兆零一千零一点
+‘99999999999999999’         >> 九万九千九百九十九兆九千九百九十九亿九千九百九十九万九千九百九十九
+"""
+
+import sys
+import re
+# import ipdb
+
+
+digit = ["〇", "一", "二", "三", "四", "五", "六", "七", "八", "九"]
+unit = ["", "十", "百", "千", "万", "亿", "兆"]  # 兆=10^12
+unit_minor = ["十", "百", "千", "万"]  # "万" only apply for "兆"
+decimal_dot = '点'
+sign_positive = '正'
+sign_negative = '负'
+plus_minus = '正负'
+
+
+def int2zh(num):
+    """num is string,
+    either the integer part of a float or just an integer itself
+    return the counterpart string of its Chinese version.
+    """
+    if float(num) == 0:
+        return digit[0]
+
+    digit_count = 0
+    s = ''
+    p0 = [0]  # if "万", "亿", "兆" is ZERO, record digit_count position
+
+    num_list = list(num)
+    while len(num_list) > 0:
+        d = int(num_list.pop())
+        if (d % 10) > 0:
+            if (digit_count <= 4):
+                s += f"{unit[digit_count]}{digit[d % 10]}"
+            elif digit_count in range(5, 8):
+                if re.search(unit[4], s) is None:
+                    s += unit[4]
+                s += f"{unit_minor[digit_count - 5]}{digit[d % 10]}"
+
+            elif (digit_count == 8):
+                s += f"{unit[5]}{digit[d % 10]}"
+            elif digit_count in range(9, 12):
+                if re.search(unit[5], s) is None:
+                    s += unit[5]
+                s += f"{unit_minor[digit_count - 9]}{digit[d % 10]}"
+
+            elif (digit_count == 12):
+                s += f"{unit[6]}{digit[d % 10]}"
+            elif digit_count in range(13, 17):
+                if re.search(unit[6], s) is None:
+                    s += unit[6]
+                s += f"{unit_minor[digit_count - 13]}{digit[d % 10]}"
+
+            else:
+                raise ValueError('Overflow!')
+
+        else:
+            # When to add digit ZERO is tricky!
+            if len(s) > 0:
+                if s[-1] != digit[0]:
+                    if digit_count in [4, 8, 12]:
+                        p0.append(digit_count)
+                    else:
+                        s += digit[0]
+
+        digit_count += 1
+    # print('List special ZERO positions: ', p0)
+    return re.sub("^一十", '十', s[::-1])
+
+
+def getsign(num):
+    """input the raw num string, return a tuple (sign_num, num).
+    """
+    sign_num = ''
+    if num.startswith('±'):
+        sign_num = plus_minus
+        num = num.lstrip('±')
+
+    try:
+        temp = float(num)
+        if (temp < 0) and (sign_num == ''):
+            sign_num = sign_negative
+        elif (temp > 0) and (sign_num == ''):
+            if ('+' in num):
+                sign_num = sign_positive
+        else:
+            if num.startswith('-'):
+                sign_num = sign_negative
+            if num.startswith('+'):
+                sign_num = sign_positive
+        num = num.lstrip('+-')
+    except ValueError:
+        raise
+    print("sign_num, num: ", sign_num, num)
+    return sign_num, num
+
+
+def num2zh(num, sep='', significant=False):
+    """main function. Default separators `sep` of digits is None.
+    Set `significant` as True to exclude insignificant figures.
+    """
+    sign_num = ''
+
+    if sep:
+        num = ''.join(num.split(sep))
+    num = re.sub('\s+', '', num)  # reassure no space
+
+    sign_num, num = getsign(num)
+
+    if '.' in num:
+        integers, remainders = num.split('.')
+        # ipdb.set_trace()
+        if integers.isdigit():
+            if not significant and int(integers) == 0:
+                str_int = ''.join([digit[0] for d in integers])
+            else:
+                str_int = int2zh(integers)
+        else:
+            str_int = ''
+
+        str_remainder = ''.join([digit[int(x)] for x in remainders])
+        return sign_num + str_int + decimal_dot + str_remainder
+    else:
+        if not significant and int(num) == 0:
+            return sign_num + ''.join([digit[0] for d in num])
+        else:
+            return sign_num + int2zh(num)
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        line = line.strip()
+        if line == '':
+            break
+        print(num2zh(line.strip()))
